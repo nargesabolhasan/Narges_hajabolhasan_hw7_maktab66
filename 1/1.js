dc338eb5b1519ee74af06806d56b92a499c93142
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-21 13:51:49 +0330

    all folders created

---
+++ 1/1.js
@@ -0,0 +1,12 @@
+//بخش الف:
+function Lesson (name, capacity,unit){
+    this.lessonName=name;
+    this.capacity=capacity;
+    this.unit=unit;
+    this.poly=function(){
+    }
+   
+    
+    // بخش ب :
+    
+    

fa3619f51b39e6bd37060b96eb34f30e7799a3b3
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-21 13:54:25 +0330

    2 object created for Lesson

--- 1/1.js
+++ 1/1.js
@@ -5,6 +5,16 @@ function Lesson (name, capacity,unit){
     this.unit=unit;
     this.poly=function(){
     }
+    this.info=function () {
+        return `{className:${this.lessonName}, classUnit:${this.unit} ,classcapacity: ${this.capacity},}`
+        };
+        }
+        let softEngineering= new Lesson ("softEngineering" ,50,3,"p")
+        let networkClass= new Lesson ("networkClass" , 30,3,"p")
+        
+        console.log(softEngineering.info() ) 
+        console.log( networkClass.info())
+    
    
     
     // بخش ب :

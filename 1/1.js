5cb2fa69e6e4f6290cac5ffb74be3ccd3886f55e
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-22 21:19:36 +0330

    part2:poly added

--- 1/1.js
+++ 1/1.js
@@ -1,20 +1,32 @@
 
 //بخش الف
-function Lesson (name, capacity,unit){
+// function Lesson (name, capacity,unit){
+//     this.lessonName=name;
+//     this.capacity=capacity;
+//     this.unit=unit;
+//     this.info=function () {
+//         return `{className:${this.lessonName}, classUnit:${this.unit} ,classcapacity: ${this.capacity} }`
+//         };
+//     }
+//     let softEngineering= new Lesson ("softEngineering" ,50,3,"p")
+//     let networkClass= new Lesson ("networkClass" , 30,3,"p")
+    
+//     console.log( softEngineering.info()) 
+//     console.log( networkClass.info())
+
+  //بخش ب
+  function Lesson (name, capacity,unit){
     this.lessonName=name;
     this.capacity=capacity;
     this.unit=unit;
-    this.info=function () {
-        return `{className:${this.lessonName}, classUnit:${this.unit} ,classcapacity: ${this.capacity} }`
-        };
-    }
-    let softEngineering= new Lesson ("softEngineering" ,50,3,"p")
-    let networkClass= new Lesson ("networkClass" , 30,3,"p")
-    
-    console.log( softEngineering.info()) 
-    console.log( networkClass.info())
+    this.poly=function(prop) {
+        if(this===softEngineering){
+            this.book=prop
+        }else{
+            this.project=prop
+        }
+    }        
 
-  
     
        
 

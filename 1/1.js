150353824580092d83f5af2264c2ea646c5e2732
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-22 21:21:09 +0330

    part2:finished

--- 1/1.js
+++ 1/1.js
@@ -30,6 +30,12 @@
         return `{className:${this.lessonName}, classUnit:${this.unit} ,classcapacity: ${this.capacity},${this.book?" book:true" : ""}${this.project?" project:true" : ""} }`
         };
     }
+    let softEngineering= new Lesson ("softEngineering" ,50,3,"p")
+    let networkClass= new Lesson ("networkClass" , 30,3,"p")
+    softEngineering.poly("true")
+    networkClass.poly("true")
+    console.log( softEngineering.info()) 
+    console.log( networkClass.info())
 
     
        

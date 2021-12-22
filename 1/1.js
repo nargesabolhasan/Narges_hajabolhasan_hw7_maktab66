acdaa1bcc3d41637591ca59307c479566e3bdadf
narges Hajabolhasan <narges.abolhasan@gmail.com>
2021-12-22 21:15:48 +0330

    part 1:function constructor created

--- 1/1.js
+++ 1/1.js
@@ -1,22 +1,19 @@
-//بخش الف:
+
+//بخش الف
 function Lesson (name, capacity,unit){
     this.lessonName=name;
     this.capacity=capacity;
     this.unit=unit;
-    this.poly=function(){
-    }
     this.info=function () {
-        return `{className:${this.lessonName}, classUnit:${this.unit} ,classcapacity: ${this.capacity},}`
+        return `{className:${this.lessonName}, classUnit:${this.unit} ,classcapacity: ${this.capacity} }`
         };
-        }
-        let softEngineering= new Lesson ("softEngineering" ,50,3,"p")
-        let networkClass= new Lesson ("networkClass" , 30,3,"p")
-        
-        console.log(softEngineering.info() ) 
-        console.log( networkClass.info())
+    }
+     
+  
     
-   
+       
+
+  
     
-    // بخش ب :
-    
+ 
     
